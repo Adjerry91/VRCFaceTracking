7bc450c75fe1daee2d0ad2ee49b7bed1ba6c349b
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-09-18 14:02:19 -0500

    More ML

--- VRCFaceTracking/ML/CSVManager.cs
+++ VRCFaceTracking/ML/CSVManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -13,19 +15,31 @@ namespace VRCFaceTracking.ML
         private const string LOG_DIRECTORY_NAME = "VRCFaceTracking Logs";
         private const string delimiter = ",";
         private static string logDirectoryPath;
+        private static string eyeImageDirectory;
+        private static string lipImageDirectory;
 
         private static StringBuilder sb;
         private static StreamWriter writer;
 
+        private static MemoryStream eyeMS;
+        private static FileStream eyeFS;
+        private static MemoryStream lipMS;
+        private static FileStream lipFS;
+
         public static void Initialize(string logDirectory = null)
         {
+            if (!(UnifiedTrackingData.LatestEyeData.SupportsImage && UnifiedTrackingData.LatestLipData.SupportsImage))
+            {
+                Logger.Warning("Neither eye or lip images were detected! CSVManager will be disabled for this session");
+                return;
+            }
+
             if (writer != null || sb != null)
             {
                 Logger.Error("Can not have multiple instances of the CSVManager manager running!");
                 return;
             }
-
-            if (string.IsNullOrEmpty(logDirectory)) // Place the logs in a subfolder beneath VRCFT
+            else if (string.IsNullOrEmpty(logDirectory)) // Place the logs in a subfolder beneath VRCFT
             {
                 logDirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), LOG_DIRECTORY_NAME);
                 Directory.CreateDirectory(LOG_DIRECTORY_NAME);
@@ -40,9 +54,23 @@ namespace VRCFaceTracking.ML
                 Logger.Error("Invalid path was given. Data logging will not be active for this session.");
                 return;
             }
-            
+
+            var context = SanitizedLogName(DateTime.UtcNow.ToString());
+
+            eyeImageDirectory = Path.Combine(logDirectoryPath, context, "eyeImages");
+            Directory.CreateDirectory(eyeImageDirectory);
+            lipImageDirectory = Path.Combine(logDirectoryPath, context, "lipImages");
+            Directory.CreateDirectory(lipImageDirectory);
+
+            // Will FS be overriden? Do we need to make a new stream per update?
+            // CreateNew will throw an exception if this is the case. TODO Test!
+            eyeMS = new MemoryStream(UnifiedTrackingData.LatestEyeData.ImageData);
+            eyeFS = new FileStream(eyeImageDirectory, FileMode.CreateNew);
+            lipMS = new MemoryStream(UnifiedTrackingData.LatestLipData.ImageData);
+            lipFS = new FileStream(lipImageDirectory, FileMode.CreateNew);
+
             writer = new StreamWriter(new FileStream(
-                Path.Combine(logDirectoryPath, $"{Environment.MachineName} - v{CSV_VERSION} - {SanitizedLogName(DateTime.UtcNow.ToString())}.csv"),
+                Path.Combine(logDirectoryPath, $"{Environment.MachineName} - v{CSV_VERSION} - {context}.csv"),
                 FileMode.Create, 
                 FileAccess.Write));
             writer.AutoFlush = true;
@@ -50,18 +78,19 @@ namespace VRCFaceTracking.ML
             sb = new StringBuilder();
             LabelColumns();
             
-            UnifiedTrackingData.OnUnifiedDataUpdated += Log;
-            
-            writer.WriteLine(sb.ToString());
-            sb.Clear();
+            UnifiedTrackingData.OnUnifiedDataUpdated += Collect;
         }
 
         private static void LabelColumns()
         {
             sb.Append("dateTime");
             sb.Append(delimiter);
+
             LabelEyes();
             LabelLips();
+
+            writer.WriteLine(sb.ToString());
+            sb.Clear();
         }
 
         private static void LabelEyes()
@@ -90,6 +119,12 @@ namespace VRCFaceTracking.ML
 
             sb.Append("eye.eyeData.EyesPupilDiameter");
             sb.Append(delimiter);
+
+            sb.Append("eye.SupportsImage");
+            sb.Append(delimiter);
+
+            sb.Append("eye.ImagePath");
+            sb.Append(delimiter);
         }
 
         private static void LabelLips()
@@ -100,21 +135,28 @@ namespace VRCFaceTracking.ML
                 sb.Append(string.Join(delimiter, $"lip.{lipShapes[i]}"));
                 sb.Append(delimiter);
             }
-        }
 
-        private static void Log(EyeTrackingData eyeData, LipTrackingData lipData)
-        {
-            sb.Append(DateTime.Now.ToString());
+            sb.Append("lip.SupportsImage");
             sb.Append(delimiter);
 
-            CollectEyes(eyeData);
-            CollectLips(lipData);
+            sb.Append("lip.ImagePath");
+            sb.Append(delimiter);
+        }
+
+        private static void Collect(EyeTrackingData eyeData, LipTrackingData lipData)
+        {
+            var dateTime = SanitizedLogName(DateTime.Now.ToString());
+            sb.Append(dateTime);
+            sb.Append(delimiter);
+
+            CollectEyes(eyeData, dateTime);
+            CollectLips(lipData, dateTime);
             
             writer.WriteLine(sb.ToString());
             sb.Clear();
         }
 
-        private static void CollectEyes(EyeTrackingData eyeData)
+        private static void CollectEyes(EyeTrackingData eyeData, string dateTime)
         {
             CollectEye(eyeData.Left);
             CollectEye(eyeData.Right);
@@ -124,6 +166,12 @@ namespace VRCFaceTracking.ML
             sb.Append(delimiter);
 
             sb.Append(eyeData.EyesPupilDiameter);
+            sb.Append(delimiter);
+
+            sb.Append(eyeData.SupportsImage);
+            sb.Append(delimiter);
+
+            sb.Append($"eye.Image.{dateTime}");
             sb.Append(delimiter);
         }
 
@@ -143,11 +191,22 @@ namespace VRCFaceTracking.ML
             
             sb.Append(eye.Look.y);
             sb.Append(delimiter);
+
+            eyeMS.WriteTo(eyeFS);
         }
 
-        private static void CollectLips(LipTrackingData lipData)
+        private static void CollectLips(LipTrackingData lipData, string dateTime)
         {
             sb.Append(string.Join(delimiter, lipData.LatestShapes));
+            sb.Append(delimiter);
+
+            sb.Append(lipData.SupportsImage);
+            sb.Append(delimiter);
+
+            sb.Append($"lip.Image.{dateTime}");
+            sb.Append(delimiter);
+
+            lipMS.WriteTo(lipFS);
         }
 
         private static string SanitizedLogName(string oldName)
@@ -173,6 +232,16 @@ namespace VRCFaceTracking.ML
 
             writer.Close();
             writer.Dispose();
+
+            eyeFS.Close();
+            eyeFS.Dispose();
+            eyeMS.Close();
+            eyeMS.Dispose();
+
+            lipFS.Close();
+            lipFS.Dispose();
+            lipMS.Close();
+            lipMS.Dispose();
         }
     }
 }
